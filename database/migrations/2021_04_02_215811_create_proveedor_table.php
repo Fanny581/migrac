0591705284133fcbbe6c1509d5f718c62662429b
Fanny581 <fannyyulissacruz16@gmail.com>
2021-04-02 16:04:54 -0600

    migraciones

---
+++ database/migrations/2021_04_02_215811_create_proveedor_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProveedorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proveedor', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre',50);
+            $table->string('direccion',50);
+            $table->string('telefono',20);
+            $table->string('email',20);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proveedor');
+    }
+}
---
+++ database/migrations/2021_04_02_215958_create_producto_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('producto', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('codigo',20);
+            $table->string('nombre',50);
+            $table->string('cantida',50);
+            $table->string('precio',20);
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('producto');
+    }
+}
